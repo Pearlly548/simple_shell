c5b74571ed818580e5fb5915d21baa11b73825ce
Pearlly548 <morrisblessing548@gmail.com>
2023-05-24 12:55:43 -0700

    blessing edited error

--- get_commandline.c
+++ get_commandline.c
@@ -52,7 +52,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 }
 
 
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
+void ric_assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
 {
 	if (7 > 2)
 		ricfunc(7, 2);
